f4b4a7090b7e4705dcee291ddb5c695e4283f50e
rizotas <aslheyramirez@gmail.com>
2015-01-22 16:28:04 -0500

    refactoring Notificacion

--- notificacion/urls.py
+++ notificacion/urls.py
@@ -1,10 +1,8 @@
-from rest_framework.routers import DefaultRouter
+from django.conf.urls import patterns, url
 
 from . import views
 
-#listar notificaciones
-router = DefaultRouter()
-router.register(r'^unread', views.NotificationUnRead)
-router.register(r'^all', views.NotificationLista)
-
-routerNotificacion = router.urls
+routerNotificacion = patterns(
+    '',
+    url(r'^all', views.NotificationLista.as_view()),
+    url(r'^unread', views.NotificationUnRead.as_view()), )
--- notificacion/views.py
+++ notificacion/views.py
@@ -1,15 +1,12 @@
-from rest_framework import viewsets
-from rest_framework import mixins
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
-
-from rest_framework.response import Response
 
 from notifications.models import Notification
 
 from .serializers import NotificationSerializer
 
 
-class NotificationLista(mixins.ListModelMixin, viewsets.GenericViewSet):
+class NotificationLista(ListAPIView):
     """
     Lista Todas las notificaciones del usuario que se encuentra autenticado
     """
@@ -17,28 +14,19 @@ class NotificationLista(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = {}
     serializer_class = NotificationSerializer
     permission_classes = (IsAuthenticated, )
+    paginate_by = 10
 
-    def list(self, request, *args, **kwargs):
+    def get_queryset(self):
         #obtiene todas las notificaciones del usuario autenticado
-        instances = request.user.notifications.all()
-
-        #serializa las notificaciones
-        serializer = self.get_serializer(instances, many=True)
-
-        #retorna el JSON
-        return Response(serializer.data)
+        return self.request.user.notifications.all()
 
 
 class NotificationUnRead(NotificationLista):
     """
     Lista todas las notificaciones marcadas sin leer
     """
-    def list(self, request, *args, **kwargs):
-        #obtiene todas las notificaciones sin leer del usuario autenticado
-        instances = request.user.notifications.unread().order_by('-timestamp')
+    paginate_by = 5
 
-        #serializa las notificaciones
-        serializer = self.get_serializer(instances, many=True)
-
-        #retorna el JSON
-        return Response(serializer.data)
+    def get_queryset(self):
+        #obtiene todas las notificaciones del usuario autenticado
+        return self.request.user.notifications.unread().order_by('-timestamp')
